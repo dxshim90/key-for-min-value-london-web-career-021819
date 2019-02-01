5944885e865098d3461fc4c312cb27fb6c86bd2a
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 12:02:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+name_hash = {"Dave": 21, "Susan": 25, "Alan": 30}
 
+def key_for_min_value(name_hash)
+name_hash.each do |name, age|
+  
 end
