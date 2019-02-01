bd03ee1010c7090db4d662942ee7c7c9101bb5d5
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 12:40:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(hash)
   smallest_key = nil
   smallest_value = nil
   hash.each do |key, value|
-    if smallest_value == nil || value < tiniest_value
+    if smallest_value == nil || value < smallest_value
       smallest_value = value
       smallest_key = key
     end
