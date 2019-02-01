81bbb80485732c01eec93e349e631ce4b0751a10
Daniel Shimield <dxshim90@gmail.com>
2019-02-01 12:33:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
 smallest_name = 0 
 smallest_value = 0 
 name_hash.each do |a, b|
-  if b < smallest_value || smallest_value == 0
+  if b < smallest_value || smallest_value == 0 
     smallest_value = b
     smallest_name = = a
   end
